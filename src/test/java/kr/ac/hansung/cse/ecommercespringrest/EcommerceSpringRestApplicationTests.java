715c12bba7d08da18d36fe3b62c3b0acaa218663
stelladream <neonykim@gmail.com>
2021-05-28 16:40:47 +0900

    create project

---
+++ src/test/java/kr/ac/hansung/cse/ecommercespringrest/EcommerceSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.ecommercespringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
